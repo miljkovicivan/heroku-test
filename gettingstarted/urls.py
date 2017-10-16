a5c8b155feb809999e1a1607fd57219d9709a7b7
miljkovicivan <miljkovicivann@gmail.com>
2017-10-16 21:52:17 +0200

    Added authentication classes

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,9 +10,9 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^pets/', include('pets.urls')),
-    url(r'^rest-auth/', include('rest_auth.urls')),
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Cat, Dog
 from .serializers import CatSerializer, DogSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     serializer_class = CatSerializer
+    permission_classesa = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return self.request.user.cats.all()
 
 class DogViewSet(viewsets.ModelViewSet):
     serializer_class = DogSerializer
+    permission_classesa = [permissions.IsAuthenticated]
+
     def get_queryset(self):
         return self.request.user.dogs.all()
 
